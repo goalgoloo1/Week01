31cf304f079b0fd1bf9e9ee2f66aff9cc91efd8a
YurimKim <americasso99@gmail.com>
2025-03-04 23:08:34 +0900

    add SpawnManager in git

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    GameObject _Player;
+    void Start()
+    {
+        _Player = Resources.Load<GameObject>("Prefabs/Player");
+        if (_Player)
+        {
+            Instantiate(_Player);
+        }
+        else
+        {
+            Debug.Log("Player not found!");
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
