8babe52ae01ec687976c1a8dd7777ab2fae32296
kimHanSeong <goalgoloo1@gmail.com>
2025-03-04 22:54:24 +0900

    Commit add EnemySpawner && ObjectPooling

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,15 +1,35 @@
 using UnityEngine;
 
-public class Enemy : Character
+public class Enemy : MonoBehaviour
 {
-    void Start()
+    public float speed = 3f; // 이동 속도
+    private Transform player; // 플레이어 위치 저장
+    [SerializeField]private float lifeTime = 5f; // 일정 시간이 지나면 사라짐
+
+    void OnEnable()
     {
-        movespeed = 5;
-        hp = 1;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        Invoke(nameof(DeactivateEnemy), lifeTime); // 일정 시간이 지나면 오브젝트 풀로 반환
     }
 
     void Update()
     {
-        
+        if (player != null)
+        {
+            // 플레이어를 향해 이동
+            Vector3 direction = (player.position - transform.position).normalized;
+            transform.position += direction * speed * Time.deltaTime;
+        }
+    }
+
+    void DeactivateEnemy()
+    {
+        ObjectPooling.ReturnEnemy(gameObject);
+    }
+
+    void OnDisable()
+    {
+        CancelInvoke(); // 비활성화될 때 Invoke 취소
     }
 }
+
