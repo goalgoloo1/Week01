b5b1236760bf0fd488df41a7567173835341d502
kimHanSeong <goalgoloo1@gmail.com>
2025-03-05 11:33:16 +0900

    add enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -24,25 +24,31 @@ public class Enemy : MonoBehaviour
         if (player == null) return;
 
         float distance = Vector3.Distance(transform.position, player.position);
+        Debug.Log(distance);
 
-        if (distance > 50f)
+        if (distance > 20f) //거리가 *f을 넘으면 자유로히움직임
         {
             FreeMove();
         }
-        else if (distance <= 30f && distance > 20f)
+        else if (distance <= 20f && distance > 10f) //거리가 *f과 *f사이면 플레이어추적
         {
             ChasePlayer();
         }
-        else if (distance <= 20f)
+        else if (distance <= 10f) //거리가 *f보다 작으면 격발
         {
-            ChasePlayer();
+            RotateEnemy();
             if (!isFiring)
-            {
+            {                                  
                 StartCoroutine(FireAtPlayer());
             }
         }
     }
 
+    void RotateEnemy() 
+    {
+        Vector3 direction = (player.position - transform.position).normalized;
+        transform.up = direction; // 플레이어를 향해 회전
+    }
     // 자유롭게 이동 및 회전
     void FreeMove()
     {
@@ -65,7 +71,6 @@ public class Enemy : MonoBehaviour
         Vector3 direction = (player.position - transform.position).normalized;
         transform.position += direction * speed * Time.deltaTime;
         transform.up = direction; // 플레이어를 향해 회전
-        Debug.Log(direction);
     }
 
     // 3초마다 플레이어에게 총알 발사
